20033839e13fc9cf06facba57df687649c633d3c
John Emmons <emmons.john@gmail.com>
2017-03-19 19:13:08 +0000

    update demo code

--- demo/raspberry-pi/spi.c
+++ demo/raspberry-pi/spi.c
@@ -13,38 +13,20 @@ int main (void)
   }
   printf("initialized the driver: fd=%i\n", fd);
 
-  unsigned char data[5];
-  /* data[0] = 'j'; */
-  /* data[1] = 'o'; */
-  /* data[2] = 'h'; */
-  /* data[3] = 'n'; */
-  /* data[4] = '\0'; */
-  data[0] = 0xff;
-  data[1] = 0x0;
-  printf("about to send the buffer: %s\n", (char*) data);
-
   int i;
+  unsigned char data[1];
   for( i = 0; i < 100000; i++ ){
 
-    //system("gpio edge 0 falling"
-    data[0] = 0x80;
-    //fprintf(stderr, "sending %u\n", data[0]);
-    if( wiringPiSPIDataRW(0, data, 1) == -1 ){
-      fprintf(stderr, "ERROR: data transfer error\n");
-      return -1;    
-    }
-    //printf("received %u\n", data[0]);
-    //delay(1000);
-      
-    data[0] = 1;
-    //fprintf(stderr, "sending %u\n", data[0]);
-    if( wiringPiSPIDataRW(0, data, 1) == -1 ){
-      fprintf(stderr, "ERROR: data transfer error\n");
-      return -1;    
-    }
-    //printf("received %u\n", data[0]);
-    //delay(1000);
+    data[0] = i % 16;
     
+    //system("gpio edge 0 falling"
+    fprintf(stderr, "sending %u\n", data[0]);
+    if( wiringPiSPIDataRW(0, data, 1) == -1 ){
+      fprintf(stderr, "ERROR: data transfer error\n");
+      return -1;    
+    }
+    //printf("received %u\n", data[0]);
+    delay(1000);
   }
   
   printf("received the buffer: %s\n", (char*) data);
